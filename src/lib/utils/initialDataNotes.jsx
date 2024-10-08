d2b062a5e6b535edefd294e528122466db23164b
KahfiSmith <alkahfii2018@gmail.com>
2024-10-08 13:22:30 +0700

    revision(note): modifikasi initial data

--- src/lib/utils/initialDataNotes.jsx
+++ src/lib/utils/initialDataNotes.jsx
@@ -1,46 +1,44 @@
-import { generateNoteId } from "./generateNoteId";
-
 export const initialNotesData = [
   {
-    id: generateNoteId() + 1,
+    id: 1,
     title: "Babel",
     body: "Babel merupakan tools open-source yang digunakan untuk mengubah sintaks ECMAScript 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.",
-    createdAt: "Minggu, 6 Oktober 2024",
+    createdAt: "2022-04-14T04:27:34.572Z",
     archived: false,
   },
   {
-    id: generateNoteId() + 2,
+    id: 2,
     title: "Functional Component",
     body: "Functional component merupakan React component yang dibuat menggunakan fungsi JavaScript. Agar fungsi JavaScript dapat disebut component ia harus mengembalikan React element dan dipanggil layaknya React component.",
-    createdAt: "Senin, 7 Oktober 2024",
+    createdAt: "2022-04-14T04:27:34.572Z",
     archived: false,
   },
   {
-    id: generateNoteId() + 3,
+    id: 3,
     title: "Modularization",
     body: "Dalam konteks pemrograman JavaScript, modularization merupakan teknik dalam memecah atau menggunakan kode dalam berkas JavaScript secara terpisah berdasarkan tanggung jawabnya masing-masing.",
-    createdAt: "Selasa, 8 Oktober 2024",
+    createdAt: "2022-04-14T04:27:34.572Z",
     archived: false,
   },
   {
-    id: generateNoteId() + 4,
+    id: 4,
     title: "Lifecycle",
     body: "Dalam konteks React component, lifecycle merupakan kumpulan method yang menjadi siklus hidup mulai dari component dibuat (constructor), dicetak (render), pasca-cetak (componentDidMount), dan sebagainya. ",
-    createdAt: "Rabu, 9 Oktober 2024",
+    createdAt: "2022-04-14T04:27:34.572Z",
     archived: false,
   },
   {
-    id: generateNoteId() + 5,
+    id: 5,
     title: "ESM",
     body: "ESM (ECMAScript Module) merupakan format modularisasi standar JavaScript.",
-    createdAt: "Kamis, 10 Oktober 2024",
+    createdAt: "2022-04-14T04:27:34.572Z",
     archived: false,
   },
   {
     id: 6,
     title: "Module Bundler",
     body: "Dalam konteks pemrograman JavaScript, module bundler merupakan tools yang digunakan untuk menggabungkan seluruh modul JavaScript yang digunakan oleh aplikasi menjadi satu berkas.",
-    createdAt: "Jum'at, 1 Oktober 2024",
+    createdAt: "2022-04-14T04:27:34.572Z",
     archived: false,
   },
 ];
