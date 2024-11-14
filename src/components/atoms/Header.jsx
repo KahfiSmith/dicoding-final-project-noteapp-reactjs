2a458aaa25d092a3367b9745277df5eb26b19164
KahfiSmith <alkahfii2018@gmail.com>
2024-11-14 10:12:40 +0700

    change(note): change name web application

--- src/components/atoms/Header.jsx
+++ src/components/atoms/Header.jsx
@@ -3,7 +3,7 @@ import SearchNote from "./Search";
 const HeaderNote = ({ filterSearch, setFilterSearch }) => {
   return (
     <div className="flex justify-between border-b border-slate-300 p-4 items-center mb-4">
-      <h1 className="text-3xl font-semibold">NotesApp</h1>
+      <h1 className="text-3xl font-semibold">JustaNote</h1>
       <SearchNote
         filterSearch={filterSearch}
         setFilterSearch={setFilterSearch}
